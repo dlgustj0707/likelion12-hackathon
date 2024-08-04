3ff87f77ff4126d70558fa64d433e700fd0528f4
ParkKyungWan <pkw8117197@gmail.com>
2024-08-05 00:22:21 +0900

    fix: challengeViewAll_css삐딱하게_수정)

--- src/pages/challengeViewAll/challengeViewAll.jsx
+++ src/pages/challengeViewAll/challengeViewAll.jsx
@@ -12,37 +12,48 @@ function ChallengeViewAll() {
                     <hr className={styles.topTitle}/>
                     <p className={styles.subtitle}>교내 첼린지<br />전체 보기</p>
                 </div>
+
                 <div className={styles.challengeList}>
-                    <ChallengeViewComponent
-                        title="샐러디 주 5일 참석"
-                        people="53"
-                        content="여름을 다가오며 다이어트를 결심한 당신! 어쩌구저쩌구~"
-                    />
-                    <ChallengeViewComponent
-                        title="샐러디 주 5일 참석"
-                        people="53"
-                        content="여름을 다가오며 다이어트를 결심한 당신! 어쩌구저쩌구~"
-                    />
-                    <ChallengeViewComponent
-                        title="샐러디 주 5일 참석"
-                        people="53"
-                        content="여름을 다가오며 다이어트를 결심한 당신! 어쩌구저쩌구~"
-                    />
-                    <ChallengeViewComponent
-                        title="샐러디 주 5일 참석"
-                        people="53"
-                        content="여름을 다가오며 다이어트를 결심한 당신! 어쩌구저쩌구~"
-                    />
-                    <ChallengeViewComponent
-                        title="샐러디 주 5일 참석"
-                        people="53"
-                        content="여름을 다가오며 다이어트를 결심한 당신! 어쩌구저쩌구~"
-                    />
-                    <ChallengeViewComponent
-                        title="샐러디 주 5일 참석"
-                        people="53"
-                        content="여름을 다가오며 다이어트를 결심한 당신! 어쩌구저쩌구~"
-                    />
+                    <div className={styles.challengeListRow}>
+                        <ChallengeViewComponent
+                            title="샐러디 주 5일 참석"
+                            people="53"
+                            content="여름을 다가오며 다이어트를 결심한 당신! 어쩌구저쩌구~"
+                        />
+                        <ChallengeViewComponent
+                            title="샐러디 주 5일 참석"
+                            people="53"
+                            content="여름을 다가오며 다이어트를 결심한 당신! 어쩌구저쩌구~"
+                        />
+                        <ChallengeViewComponent
+                            title="샐러디 주 5일 참석"
+                            people="53"
+                            content="여름을 다가오며 다이어트를 결심한 당신! 어쩌구저쩌구~"
+                        />
+                    </div>
+                    <div className={styles.challengeListRow}>
+                        <ChallengeViewComponent
+                            title="샐러디 주 5일 참석"
+                            people="53"
+                            content="여름을 다가오며 다이어트를 결심한 당신! 어쩌구저쩌구~"
+                        />
+                        <ChallengeViewComponent
+                            title="샐러디 주 5일 참석"
+                            people="53"
+                            content="여름을 다가오며 다이어트를 결심한 당신! 어쩌구저쩌구~"
+                        />
+                        <ChallengeViewComponent
+                            title="샐러디 주 5일 참석"
+                            people="53"
+                            content="여름을 다가오며 다이어트를 결심한 당신! 어쩌구저쩌구~"
+                        />
+                        <ChallengeViewComponent
+                            title="샐러디 주 5일 참석"
+                            people="53"
+                            content="여름을 다가오며 다이어트를 결심한 당신! 어쩌구저쩌구~"
+                        />
+                        
+                    </div>
                 </div>
             </div>
         </>
